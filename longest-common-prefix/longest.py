48d1e4e516194954890d72c510ff330b11d74008
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-12 18:26:48 +0100

    longestCommonPrefix

---
+++ longest-common-prefix/longest.py
@@ -0,0 +1,18 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    
+    # Start with the first string as the prefix
+    prefix = strs[0]
+    
+    for str in strs[1:]:
+        while not str.startswith(prefix):
+            prefix=prefix[:-1]
+            if not prefix:
+                return ""
+    
+    return prefix
+
+# Test Cases
+print(longestCommonPrefix(["flower", "flow", "flight"]))  # Output: "fl"
+print(longestCommonPrefix(["dog", "racecar", "car"]))     # Output: ""
