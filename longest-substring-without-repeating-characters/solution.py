c7f0fdd96836372b122db7b3b20bbfcc9524b16d
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-18 12:36:16 +0100

    Final :)

--- longest-substring-without-repeating-characters/solution.py
+++ longest-substring-without-repeating-characters/solution.py
@@ -1,23 +1,25 @@
-def lengthOfLongestSubstring( s):
+def lengthOfLongestSubstring(s):
     """
     :type s: str
     :rtype: int
     """
-    char_set=set()
+    char_set=set() # substring set
     max_length=0
-    left=0
+    left=0 # Position of left pointer
 
     for right in range(len(s)):
+        #check if caracter is in the substring
         while s[right] in char_set:
+            #remove the characters from the left pointer and move the pointer until the set doesn t contain duplicate elements
             char_set.remove(s[left])
             left+=1
         
         char_set.add(s[right])
-        
+
         max_length=max(max_length,right-left+1)
 
-
     return max_length
+
 
 
 s = "abcabcbb"
