8ce188ae7c378db086d4b55cbea95e355fae8120
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-18 10:07:49 +0100

    solution1

---
+++ longest-substring-without-repeating-characters/solution.py
@@ -0,0 +1,33 @@
+def lengthOfLongestSubstring( s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    char_set=set()
+    max_length=0
+    left=0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left+=1
+        
+        char_set.add(s[right])
+        
+        max_length=max(max_length,right-left+1)
+
+
+    return max_length
+
+
+s = "abcabcbb"
+
+print(lengthOfLongestSubstring(s))
+
+s = "bbbbbbbbb"
+
+print(lengthOfLongestSubstring(s))
+
+s = "pwwkew"
+
+print(lengthOfLongestSubstring(s))
