7df58048d4ff00f87753a5d33290fc056531a104
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-26 00:40:50 +0100

    added tests for twosum

--- 1. two_sum/two_sum.py
+++ 1. two_sum/two_sum.py
@@ -11,8 +11,8 @@ def twoSum( nums, target):
         else:
             Map[num]=i
         
-assert twoSum([2,7,11,15], 9)==[0, 1]
+print(twoSum([2,7,11,15], 9))
 
-assert twoSum([3,2,4], 6)==[1,2]  
+print(twoSum([3,2,4], 6))
 
-assert twoSum([3,3], 6)==[0,1]  
+print(twoSum([3,3], 6))
