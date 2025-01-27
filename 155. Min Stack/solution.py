b5488bca7d21e9597213c3c64e6453045c0c39cd
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-27 19:28:25 +0100

    Optimized

--- 155. Min Stack/solution.py
+++ 155. Min Stack/solution.py
@@ -1,26 +1,32 @@
 class MinStack:
-
     def __init__(self):
+        self.min = float('inf')
         self.stack = []
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
+    def push(self, x: int) -> None:
+        if not self.stack:
+            self.stack.append(0)
+            self.min = x
+        else:
+            self.stack.append(x - self.min)
+            if x < self.min:
+                self.min = x
 
     def pop(self) -> None:
-        self.stack.pop()
+        if not self.stack:
+            return
+        
+        pop = self.stack.pop()
+        
+        if pop < 0:
+            self.min = self.min - pop
 
     def top(self) -> int:
-        return self.stack[-1]
+        top = self.stack[-1]
+        if top > 0:
+            return top + self.min
+        else:
+            return self.min
 
     def getMin(self) -> int:
-        tmp = []
-        mini = self.stack[-1]
-
-        while len(self.stack):
-            mini = min(mini, self.stack[-1])
-            tmp.append(self.stack.pop())
-        
-        while len(tmp):
-            self.stack.append(tmp.pop())
-        
-        return mini
+        return self.min
