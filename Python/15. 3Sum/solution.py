d362b7fa5d49dce21e72c92e288b2af6dd99811f
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-30 22:52:39 +0100

    3 sum map solution

--- Python/15. 3Sum/solution.py
+++ Python/15. 3Sum/solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def threeSumPointer(self, nums):
         nums.sort()
@@ -27,4 +30,26 @@ class Solution:
                         l+=1
         return res
              
-             
+    def threeSumMap(self, nums):
+        nums.sort()
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+
+        res = []
+        for i in range(len(nums)):
+            count[nums[i]] -= 1
+            if i and nums[i] == nums[i - 1]:
+                continue
+                
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] -= 1
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue
+                target = -(nums[i] + nums[j])
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] += 1
+        return res
