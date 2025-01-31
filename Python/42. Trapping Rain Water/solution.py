3ebe7475ebdd8aef18a54b350ba92e870de9ccc5
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-01-31 23:55:21 +0100

    space efficient trapping water

--- Python/42. Trapping Rain Water/solution.py
+++ Python/42. Trapping Rain Water/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def trap(self, height):
+
         maxleft=[0 for i in range(len(height))]
         maxright=[0 for i in range(len(height))]
-
         for i in range(1,len(height)):
             maxleft[i]=max(maxleft[i-1],height[i-1])
         
@@ -10,9 +10,26 @@ class Solution:
             maxright[i]=max(maxright[i+1],height[i+1])
         
         water=0
-        
         for i in range(len(height)):
             container=min(maxright[i],maxleft[i])-height[i]
             if container>0:
                 water+=container
         return water
+
+    def trap_efficient(self, height) :
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
