3f723c9e960267e83d6fde62959aef0aca56ae6b
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-06 20:00:09 +0100

    recursion eval RPN

--- Python/150. Evaluate Reverse Polish Notation/solution.py
+++ Python/150. Evaluate Reverse Polish Notation/solution.py
@@ -15,3 +15,23 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
+
+    def evalRPN_rec(self, tokens):
+        def dfs():
+            token = tokens.pop()
+            if token not in "+-*/":
+                return int(token)
+            
+            right = dfs()
+            left = dfs()
+            
+            if token == '+':
+                return left + right
+            elif token == '-':
+                return left - right
+            elif token == '*':
+                return left * right
+            elif token == '/':
+                return int(left / right)
+        
+        return dfs()
