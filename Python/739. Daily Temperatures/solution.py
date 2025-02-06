69e8f69c5ebecd0ccc797daf7284ed9c54d99a3e
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-06 19:56:51 +0100

    dynamic ssolution

--- Python/739. Daily Temperatures/solution.py
+++ Python/739. Daily Temperatures/solution.py
@@ -14,3 +14,19 @@ class Solution:
             if j < n:
                 res[i] = j - i
         return res
+    
+    def dailyTemperatures_dynamic(self, temperatures):
+        n = len(temperatures)
+        res = [0] * n
+
+        for i in range(n - 2, -1, -1):
+            j = i + 1
+            while j < n and temperatures[j] <= temperatures[i]:
+                if res[j] == 0:
+                    j = n
+                    break
+                j += res[j]
+            
+            if j < n:
+                res[i] = j - i
+        return res
