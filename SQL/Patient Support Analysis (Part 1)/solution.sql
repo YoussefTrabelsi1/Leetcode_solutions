fa09afa42a837f31ecdb7868e56312961890ddf2
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-19 00:04:07 +0100

    sol Patient Support Analysis (Part 1)

---
+++ SQL/Patient Support Analysis (Part 1)/solution.sql
@@ -0,0 +1,12 @@
+WITH calls_per_holder AS (
+  SELECT 
+    policy_holder_id,
+    COUNT(DISTINCT case_id) AS policy_holder_count
+  FROM callers 
+  GROUP BY policy_holder_id
+  HAVING COUNT(DISTINCT case_id)>2
+)
+
+SELECT 
+  count(policy_holder_count) AS policy_holder_count
+FROM calls_per_holder
