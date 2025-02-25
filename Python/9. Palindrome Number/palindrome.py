241e0daa204f167fa3b7e1520e98cff9b6f0d412
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-25 22:05:03 +0100

    palindrome

--- Python/9. Palindrome Number/palindrome.py
+++ Python/9. Palindrome Number/palindrome.py
@@ -1,17 +1,17 @@
 def isPalindrome(x):
     # Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
-    if x < 0 or (x % 10 == 0 and x != 0):
+    if x<0  or (x % 10 == 0 and x != 0):
         return False
     
-    reversed_half = 0
-    while x > reversed_half:
-        # Add the last digit to reversed_half
-        reversed_half = reversed_half * 10 + x % 10
-        # Remove the last digit from x
-        x //= 10
+    reversed=0
+    while reversed<x:
+
+        reversed=10*reversed + x%10
+        x//=10
+    print(reversed)
     
-    # Check if either the full reversed half matches, or if it's the same ignoring the middle digit (for odd lengths)
-    return x == reversed_half or x == reversed_half // 10
+    return x==reversed or x == reversed//10
+
 
 print(isPalindrome(121))
 print(isPalindrome(-121))
