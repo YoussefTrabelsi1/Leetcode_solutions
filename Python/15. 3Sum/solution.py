8a1ac5ab770a20199bb29c281e2d88eab5c3f00f
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-25 22:07:00 +0100

    3 sum

--- Python/15. 3Sum/solution.py
+++ Python/15. 3Sum/solution.py
@@ -7,23 +7,15 @@ class Solution:
 
         res=[]
 
-        for i,a in enumerate(nums):
-            if a>0:
-                break
-            if i >0 and a==nums[i-1]:
-                continue
-            
+        for i in range(len(nums)-1):
             l,r=i+1,len(nums)-1
-
             while l<r:
-                threeSum=a+nums[l]+nums[r]
-
-                if threeSum<0:
+                if nums[l]+nums[r]+nums[i]<0:
                     l+=1
-                elif threeSum>0:
+                elif nums[l]+nums[r]+nums[i]>0:
                     r-=1
                 else:
-                    res.append([a,nums[l],nums[r]])
+                    res.append([nums[i],nums[l],nums[r]])
                     l+=1
                     r-=1
                     while nums[l]==nums[l-1] and l<r:
