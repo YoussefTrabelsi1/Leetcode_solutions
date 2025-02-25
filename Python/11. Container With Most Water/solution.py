e9d07c2a77461e9a98160517dfed3eb025445b5e
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-25 22:06:23 +0100

    water

--- Python/11. Container With Most Water/solution.py
+++ Python/11. Container With Most Water/solution.py
@@ -6,7 +6,6 @@ def maxArea(heights):
     l,r=0,len(heights)-1
     max_area=0
     while l<r:
-        
         max_area=max(max_area,(r-l)*min(heights[l],heights[r]))
         if heights[l]<heights[r]:
             l+=1
