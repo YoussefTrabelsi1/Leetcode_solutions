79b451cc9a3e3912b83d3e8996f85816c5c1c106
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-27 20:19:48 +0100

    product array

--- Python/238. Product of Array Except Self/solution.py
+++ Python/238. Product of Array Except Self/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def productExceptSelf(self, nums):
-        post_fix,pre_fix=[1 for k in range(len(nums)+1)],[1 for k in range(len(nums)+1)]
 
+        post_fix,pre_fix=[1 for k in range(len(nums)+1)],[1 for k in range(len(nums)+1)]
         for i in range(len(nums)):
             pre_fix[i+1]=pre_fix[i]* nums[i]
             post_fix[len(nums)-i-1]=post_fix[len(nums)-i]* nums[len(nums)-i-1]
