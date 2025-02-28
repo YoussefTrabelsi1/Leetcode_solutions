9c941275c27a499346baa3da80acd1bf282a7088
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-02-28 22:34:09 +0100

    encode decode

--- Python/271. Encode and Decode Strings/solution.py
+++ Python/271. Encode and Decode Strings/solution.py
@@ -7,9 +7,9 @@ class Solution:
         
         return res
 
-    def decode(self, s):    
-        res=[]
+    def decode(self, s):
 
+        res=[]
         for i in range(len(s)-1):
             if s[i] in [str(k) for k in range(10)] and s[i+1]=="#":
                 res.append(s[i+2:i+2+int(s[i])])
