db9ecd78b61594072652f35857279a27421c8f33
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-07 21:40:47 +0100

    3-Topping Pizzas

---
+++ solution.sql
@@ -0,0 +1,33 @@
+WITH RECURSIVE all_toppings AS (
+SELECT
+  topping_name::VARCHAR,
+  ingredient_cost::DECIMAL AS total_cost,
+  1 AS topping_numbers
+FROM pizza_toppings
+
+UNION ALL
+
+SELECT
+  CONCAT(addon.topping_name, ',', anchor.topping_name) AS topping_name,
+  addon.ingredient_cost + anchor.total_cost AS total_cost,
+  topping_numbers + 1
+FROM 
+  pizza_toppings AS addon,
+  all_toppings AS anchor
+WHERE anchor.topping_name < addon.topping_name
+), 
+arrange AS (
+SELECT
+  topping_name,
+  UNNEST(STRING_TO_ARRAY(topping_name, ',')) AS single_topping,
+  total_cost
+FROM all_toppings
+WHERE topping_numbers = 3
+)
+
+SELECT
+  STRING_AGG(single_topping, ',' ORDER BY single_topping) AS pizza,
+  total_cost
+FROM arrange
+GROUP BY topping_name, total_cost
+ORDER BY total_cost DESC, pizza;
