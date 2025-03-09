9fb4d6d3082e3356f6ddb028adf4ed7d4632b9e4
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-09 19:54:54 +0100

    sol3

--- Python/Invert Binary Tree/solution.py
+++ Python/Invert Binary Tree/solution.py
@@ -28,3 +28,16 @@ class Solution:
         self.invertTree(root.right)
         
         return root
+    
+    def invertTree(self, root):
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
