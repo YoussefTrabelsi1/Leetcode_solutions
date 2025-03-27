2afe3bcede210e43e6c9197df9ec1dea5d867717
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-27 21:40:54 +0100

    hash map

--- Python/Implement Trie (Prefix Tree)/solution.py
+++ Python/Implement Trie (Prefix Tree)/solution.py
@@ -33,3 +33,32 @@ class PrefixTree:
                 return False
             cur = cur.children[i]
         return True
+
+
+class PrefixTree_Hash_Map:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
