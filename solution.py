e745fadf39614046034527a97d5fa86c56d9481e
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-03-29 21:46:31 +0100

    debuggin

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    def kClosest(self, points, k: int):
         points.sort(key=lambda p: p[0]**2 + p[1]**2)
         return points[:k]
