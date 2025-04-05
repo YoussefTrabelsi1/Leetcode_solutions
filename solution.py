4395c5ef7068df2a8885f4b63fc24e7f38bcb473
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-05 18:41:37 +0200

    [Find the Duplicate Number] Binary Search

--- solution.py
+++ solution.py
@@ -32,3 +32,18 @@ class Solution:
                 return abs(num)
             nums[idx] *= -1
         return -1
+
+class Solution:
+    def findDuplicate(self, nums) -> int:
+        n = len(nums)
+        low, high = 1, n - 1
+        while low < high:
+            mid = low + (high - low) // 2
+            lessOrEqual = sum(1 for num in nums if num <= mid)
+
+            if lessOrEqual <= mid:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
