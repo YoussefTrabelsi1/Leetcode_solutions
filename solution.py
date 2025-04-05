cc1300ec482aa507456d9b0e35d3358736e127c1
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-05 18:40:19 +0200

    [Find the Duplicate Number] Array

--- solution.py
+++ solution.py
@@ -14,3 +14,12 @@ class Solution:
                 return num
             seen.add(num)
         return -1
+
+class Solution:
+    def findDuplicate(self, nums) -> int:
+        seen = [0] * len(nums)
+        for num in nums:
+            if seen[num - 1]:
+                return num
+            seen[num - 1] = 1
+        return -1
