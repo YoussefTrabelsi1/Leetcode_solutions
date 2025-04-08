7bfd506641cb9efe559980ece81ed4e9b62d29a1
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-08 20:43:56 +0200

    [Meeting Rooms II] Greedy

--- Python/Meeting Rooms II/solution.py
+++ Python/Meeting Rooms II/solution.py
@@ -51,3 +51,18 @@ class Solution:
                 count -= 1
             res = max(res, count)
         return res
+    
+class Solution:
+    def minMeetingRooms(self, intervals) -> int:
+        time = []
+        for i in intervals:
+            time.append((i.start, 1))
+            time.append((i.end, -1))
+        
+        time.sort(key=lambda x: (x[0], x[1]))
+        
+        res = count = 0
+        for t in time:
+            count += t[1]
+            res = max(res, count)
+        return res
