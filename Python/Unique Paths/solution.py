d29b9309cd3c2d4bc9a8c1a9c906d4f5fa6a2423
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-09 13:40:15 +0200

    [Unique Paths] Dynamic Programming (Optimal)

--- Python/Unique Paths/solution.py
+++ Python/Unique Paths/solution.py
@@ -47,3 +47,12 @@ class Solution:
                 newRow[j] = newRow[j + 1] + row[j]
             row = newRow
         return row[0]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                dp[j] += dp[j + 1]
+                
+        return dp[0]
