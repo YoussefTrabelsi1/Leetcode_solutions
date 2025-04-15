95c049655a14dd6004c61808eabee578c1b5ced2
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-15 22:20:31 +0200

    [Copy Linked List with Random Pointer] Hash Map (One Pass)

--- Python/Copy Linked List with Random Pointer/solution.py
+++ Python/Copy Linked List with Random Pointer/solution.py
@@ -1,5 +1,6 @@
 
 # Definition for a Node.
+import collections
 from typing import Optional
 
 
@@ -40,5 +41,18 @@ class Solution:
             copy = oldToCopy[cur]
             copy.next = oldToCopy[cur.next]
             copy.random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = collections.defaultdict(lambda: Node(0))
+        oldToCopy[None] = None
+        
+        cur = head
+        while cur:
+            oldToCopy[cur].val = cur.val
+            oldToCopy[cur].next = oldToCopy[cur.next]
+            oldToCopy[cur].random = oldToCopy[cur.random]
             cur = cur.next
         return oldToCopy[head]
