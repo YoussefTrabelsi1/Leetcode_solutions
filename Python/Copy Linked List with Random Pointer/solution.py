b36d206e029df693be268bbff383d5c865e1571b
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-15 22:20:53 +0200

    [Copy Linked List with Random Pointer] Space Optimized - I

--- Python/Copy Linked List with Random Pointer/solution.py
+++ Python/Copy Linked List with Random Pointer/solution.py
@@ -56,3 +56,33 @@ class Solution:
             oldToCopy[cur].random = oldToCopy[cur.random]
             cur = cur.next
         return oldToCopy[head]
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        
+        l1 = head
+        while l1 is not None:
+            l2 = Node(l1.val)
+            l2.next = l1.next
+            l1.next = l2
+            l1 = l2.next
+            
+        newHead = head.next
+        
+        l1 = head
+        while l1 is not None:
+            if l1.random is not None:
+                l1.next.random = l1.random.next
+            l1 = l1.next.next
+            
+        l1 = head
+        while l1 is not None:
+            l2 = l1.next
+            l1.next = l2.next
+            if l2.next is not None:
+                l2.next = l2.next.next
+            l1 = l1.next
+            
+        return newHead
