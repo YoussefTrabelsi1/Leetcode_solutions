d3bbcadb051b10b8aa2b93b9c7097c1b74860630
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-17 22:06:03 +0200

    [Merge K Sorted Linked Lists] Heap

--- Python/Merge K Sorted Linked Lists/solution.py
+++ Python/Merge K Sorted Linked Lists/solution.py
@@ -1,4 +1,5 @@
 # Definition for singly-linked list.
+import heapq
 from typing import List, Optional
 
 
@@ -71,3 +72,33 @@ class Solution:
         if l2:
             tail.next = l2
         return dummy.next
+
+class NodeWrapper:
+    def __init__(self, node):
+        self.node = node
+
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+
+        res = ListNode(0)
+        cur = res
+        minHeap = []
+
+        for lst in lists:
+            if lst is not None:
+                heapq.heappush(minHeap, NodeWrapper(lst))
+
+        while minHeap:
+            node_wrapper = heapq.heappop(minHeap)
+            cur.next = node_wrapper.node
+            cur = cur.next
+            
+            if node_wrapper.node.next:
+                heapq.heappush(minHeap, NodeWrapper(node_wrapper.node.next))
+        
+        return res.next
