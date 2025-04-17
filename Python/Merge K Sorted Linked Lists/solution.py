2ff50cbaf44b7283d752ec965b900972da5a7887
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-17 22:04:45 +0200

    [Merge K Sorted Linked Lists] Iteration

--- Python/Merge K Sorted Linked Lists/solution.py
+++ Python/Merge K Sorted Linked Lists/solution.py
@@ -22,3 +22,24 @@ class Solution:
             cur.next = ListNode(node)
             cur = cur.next
         return res.next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        res = ListNode(0)
+        cur = res
+        
+        while True:
+            minNode = -1
+            for i in range(len(lists)):
+                if not lists[i]:
+                    continue
+                if minNode == -1 or lists[minNode].val > lists[i].val:
+                    minNode = i
+            
+            if minNode == -1:
+                break
+            cur.next = lists[minNode]
+            lists[minNode] = lists[minNode].next
+            cur = cur.next
+
+        return res.next
