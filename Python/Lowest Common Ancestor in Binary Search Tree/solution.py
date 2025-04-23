4cf036f0c63559cb2928ad8cfb3f4275cfe4c380
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-23 09:15:18 +0200

    [Lowest Common Ancestor in Binary Search Tree] recursion

--- Python/Lowest Common Ancestor in Binary Search Tree/solution.py
+++ Python/Lowest Common Ancestor in Binary Search Tree/solution.py
@@ -3,3 +3,15 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+    
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or not p or not q:
+            return None
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
