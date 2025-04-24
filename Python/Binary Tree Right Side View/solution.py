2bcd4701039f4ee397d477ef90ca737b4ce26601
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-24 19:10:07 +0200

    [Binary Tree Right Side View] Breadth First Search

--- Python/Binary Tree Right Side View/solution.py
+++ Python/Binary Tree Right Side View/solution.py
@@ -23,3 +23,23 @@ class Solution:
         
         dfs(root, 0)
         return res
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
