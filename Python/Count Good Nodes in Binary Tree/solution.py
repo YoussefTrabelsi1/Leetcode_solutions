26c76d0797020e8b5f7a4969fd2e3e43a84805f8
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-25 20:56:08 +0200

    [Count Good Nodes in Binary Tree] Breadth First Search

--- Python/Count Good Nodes in Binary Tree/solution.py
+++ Python/Count Good Nodes in Binary Tree/solution.py
@@ -18,3 +18,23 @@ class Solution:
             return res
 
         return dfs(root, root.val)
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        res = 0
+        q = deque()
+		
+        q.append((root,-float('inf')))
+
+        while q:
+            node,maxval = q.popleft()
+            if node.val >= maxval:  
+                res += 1
+				
+            if node.left:    
+                q.append((node.left,max(maxval,node.val)))
+            
+            if node.right:
+                q.append((node.right,max(maxval,node.val)))
+                
+        return res
