d414d3ddc6bc998b559c20100d8f4355b916d4b5
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-26 11:17:03 +0200

    [Valid Binary Search Tree] Breadth First Search

--- Python/Valid Binary Search Tree/solution.py
+++ Python/Valid Binary Search Tree/solution.py
@@ -43,3 +43,21 @@ class Solution:
             )
 
         return valid(root, float("-inf"), float("inf"))
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        q = deque([(root, float("-inf"), float("inf"))])
+
+        while q:
+            node, left, right = q.popleft()
+            if not (left < node.val < right):
+                return False
+            if node.left:
+                q.append((node.left, left, node.val))
+            if node.right:
+                q.append((node.right, node.val, right))
+
+        return True
