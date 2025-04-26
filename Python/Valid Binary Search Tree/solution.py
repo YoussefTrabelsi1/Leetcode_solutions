c866c65c37da395dc265e98bb6f87e0156d26b5c
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-26 11:16:44 +0200

    [Valid Binary Search Tree] Depth First Search

--- Python/Valid Binary Search Tree/solution.py
+++ Python/Valid Binary Search Tree/solution.py
@@ -28,3 +28,18 @@ class Solution:
             return False
         return (self.isValid(root.left, limit, check) and
                 self.isValid(root.right, limit, check))
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (left < node.val < right):
+                return False
+
+            return valid(node.left, left, node.val) and valid(
+                node.right, node.val, right
+            )
+
+        return valid(root, float("-inf"), float("inf"))
