12b8059687f67ac5f50c2e7f0cbb857236767d94
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-27 20:28:39 +0200

    [Kth Smallest Integer in BST] Inorder Traversal

--- Python/Kth Smallest Integer in BST/solution.py
+++ Python/Kth Smallest Integer in BST/solution.py
@@ -23,3 +23,19 @@ class Solution:
         dfs(root)
         arr.sort()
         return arr[k - 1]
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        arr = []
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            arr.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return arr[k - 1]
