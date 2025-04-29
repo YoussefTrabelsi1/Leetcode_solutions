07210ec8498dc61da3836a60ee75f84ba672e2a2
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-04-29 17:15:51 +0200

    [Binary Tree Maximum Path Sum] Depth First Search (Optimal)

--- Python/Binary Tree Maximum Path Sum/solution.py
+++ Python/Binary Tree Maximum Path Sum/solution.py
@@ -32,3 +32,22 @@ class Solution:
         right = self.getMax(root.right)
         path = root.val + max(left, right)
         return max(0, path)
+    
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return res[0]
