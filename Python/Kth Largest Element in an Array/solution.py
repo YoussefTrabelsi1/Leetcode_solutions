5be3ecc2b6ceb7df006ddedd4fd3f7b31f178675
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-05-05 22:09:10 +0200

    [Kth Largest Element in an Array] Min-Heap

--- Python/Kth Largest Element in an Array/solution.py
+++ Python/Kth Largest Element in an Array/solution.py
@@ -5,3 +5,8 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
         return nums[len(nums) - k]
+
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        return heapq.nlargest(k, nums)[-1]
