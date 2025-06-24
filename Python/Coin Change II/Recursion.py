9bf129e0c596f62cbc4b1e9cab0485e3bc7605bd
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-06-24 22:12:11 +0200

    [Coin Change II] Recursion

---
+++ Python/Coin Change II/Recursion.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        coins.sort()
+
+        def dfs(i, a):
+            if a == 0:
+                return 1
+            if i >= len(coins):
+                return 0
+
+            res = 0
+            if a >= coins[i]:
+                res = dfs(i + 1, a)
+                res += dfs(i, a - coins[i])
+            return res
+
+        return dfs(0, amount)
