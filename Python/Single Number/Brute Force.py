7571121f9f9c25e6425d3a020e8d77e69cc9b750
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-07-22 21:32:28 +0200

    [Single Number] Brute Force

---
+++ Python/Single Number/Brute Force.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            flag = True
+            for j in range(len(nums)):
+                if i != j and nums[i] == nums[j]:
+                    flag = False
+                    break
+            if flag:
+                return nums[i]
