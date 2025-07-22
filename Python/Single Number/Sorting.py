2c5f0f7582302fd9fb06e6b53bb032ce1ee5b4b9
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-07-22 21:33:08 +0200

    [Single Number] Sorting

---
+++ Python/Single Number/Sorting.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                return nums[i]
+        return nums[i]
