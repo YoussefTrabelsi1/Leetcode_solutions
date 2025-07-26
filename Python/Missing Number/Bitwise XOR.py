594b11c96e7eab7e9ae5bf0074f36529397ea894
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-07-26 22:14:07 +0200

    [Missing Number] Bitwise XOR

---
+++ Python/Missing Number/Bitwise XOR.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xorr = n  
+        for i in range(n):
+            xorr ^= i ^ nums[i]
+        return xorr
