b34e648ba7c5d084c5808c8dc210a8cc51563e65
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-08-06 20:31:56 +0200

    [Group anagrams] Refacto

---
+++ Python/Group Anagrams/solution.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+def groupAnagrams( strs):
+        
+        c_map=defaultdict(list)
+        for string in strs:
+            count=[0]*26
+            for c in string:
+                count[ord(c)-ord('a')]+=1
+            
+            c_map[tuple(count)].append(string)
+        
+        return list(c_map.values())
