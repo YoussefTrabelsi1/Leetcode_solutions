96b64fc2c4a72db1e18f61b33b1f11805efb5a31
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-08-11 18:01:49 +0200

    [Balanced Binary Tree] Brute Force

---
+++ Python/Balanced Binary Tree/Brute force.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isBalanced_bruteforce(root: TreeNode) -> bool:
+    def height(node):
+        if not node:
+            return 0
+        return 1 + max(height(node.left), height(node.right))
+
+    if not root:
+        return True
+    return (abs(height(root.left) - height(root.right)) <= 1
+            and isBalanced_bruteforce(root.left)
+            and isBalanced_bruteforce(root.right))
