9dccf1a0d368fa5e7b987e019bb6bf4d3d67bbdc
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-09-14 21:14:34 +0200

    [Invert Binary Tree] Iterative BFS

---
+++ Python/Invert Binary Tree/Iterative BFS.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left  # swap
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return root
