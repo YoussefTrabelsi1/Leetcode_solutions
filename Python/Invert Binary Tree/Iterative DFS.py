aff49c4ab760957d8d0fc9966283e7c3072ed75b
YoussefTrabelsi1 <147254827+YoussefTrabelsi1@users.noreply.github.com>
2025-09-14 21:14:59 +0200

    [Invert Binary Tree] Iterative DFS

---
+++ Python/Invert Binary Tree/Iterative DFS.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left  # swap
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
